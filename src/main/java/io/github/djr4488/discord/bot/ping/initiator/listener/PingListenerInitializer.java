4cfdf2b86e5a00f1e9381055e02d61a631fced13
djr4488 <djr4488@gmail.com>
2019-03-25 11:23:54 -0500

    Using managed scheduled executor and managed executor, so this way don't use threads without container knowing about it

--- src/main/java/io/github/djr4488/discord/bot/ping/initiator/listener/PingListenerInitializer.java
+++ src/main/java/io/github/djr4488/discord/bot/ping/initiator/listener/PingListenerInitializer.java
@@ -1,18 +1,22 @@
 package io.github.djr4488.discord.bot.ping.initiator.listener;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.enterprise.concurrent.ManagedThreadFactory;
 import javax.inject.Inject;
 import javax.security.auth.login.LoginException;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
 import org.djr.cdi.logs.Slf4jLogger;
 import org.djr.cdi.properties.Config;
 import org.slf4j.Logger;
 
 import io.github.djr4488.discord.bot.BotException;
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDABuilder;
 
 @Startup
 @Singleton
@@ -21,10 +25,14 @@ public class PingListenerInitializer {
 	@Slf4jLogger
 	private Logger log;
 	@Inject
-	@Config(propertyName = "discord.bot.token", defaultValue = "")
+	@Config(propertyName = "discord.bot.token", defaultValue = "<<your discord bot key here>>")
 	private String discordToken;
 	@Inject
 	private PingListener pingListener;
+	@Resource(name = "scheduledExecutor")
+	private ManagedScheduledExecutorService managedScheduledExecutorService;
+	@Resource(name = "executorManaged")
+	private ManagedExecutorService managedExecutorService;
 
 	@PostConstruct
 	public void postConstruct() {
@@ -32,7 +40,10 @@ public class PingListenerInitializer {
 		try {
 			JDABuilder jdaBuilder = new JDABuilder(AccountType.BOT);
 			jdaBuilder.setToken(discordToken);
-			jdaBuilder.addEventListeners(pingListener);
+			jdaBuilder.addEventListener(pingListener);
+			jdaBuilder.setRateLimitPool(managedScheduledExecutorService);
+			jdaBuilder.setGatewayPool(managedScheduledExecutorService);
+			jdaBuilder.setCallbackPool(managedExecutorService);
 			jdaBuilder.build();
 		} catch (LoginException lEx) {
 			log.error("listen() login error", lEx);
